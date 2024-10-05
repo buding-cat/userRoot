6605cf4c9c4e8d736f28909e5b6ba8131f28e930
buding-cat <jinjianming@jinjianmingdeMacBook-Air.local>
2024-10-05 23:31:59 +0800

    first commit

---
+++ static/env.js
@@ -0,0 +1,7 @@
+// env.js
+const ENCRYPTED_URLS = {
+    mainSite: "aHR0cHM6Ly93d3cuYnVkaW5nY2F0Lnh5eg==", 
+    backupSite: "aHR0cHM6Ly9idWRpbmdjYXQxLnRvcA==", 
+    discussionGroup: "aHR0cHM6Ly90Lm1lL2J1ZGluZ2NhdA==", 
+    newsChannel: "aHR0cHM6Ly90Lm1lL2J1ZGluZ2NhdHN1Yg==",
+};
